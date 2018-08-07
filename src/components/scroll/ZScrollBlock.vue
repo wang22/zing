6ead9790322e309d610cb0b2b0f3a4970f0be97e
22 <pause22@163.com>
2018-08-07 22:35:28 +0800

    add scroll block

---
+++ src/components/scroll/ZScrollBlock.vue
@@ -0,0 +1,42 @@
+<template>
+  <div ref="sc">
+      <slot></slot>
+  </div>
+</template>
+
+<script>
+  import $ from 'jQuery'
+
+  export default {
+    name: 'ZBlock',
+    props: {
+      height: {
+        type: Number,
+        default: 0
+      }
+    },
+    mounted () {
+      require('jquery-slimscroll')
+      this.showScroll(this.height);
+    },
+    methods: {
+       showScroll: function(scHeight){
+         $(this.$refs.sc).slimScroll({
+           height: scHeight + 'px',
+           color: "#000",
+           size: "6px",
+           opacity: .35,
+           wheelStep: 15,
+           distance: "2px",
+           railVisible: !1,
+           railOpacity: 1
+         });
+       }
+    },
+    watch: {
+      height: function(newHeight,oldHeight){
+        this.showScroll(newHeight);
+      }
+    }
+  };
+</script>
