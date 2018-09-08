f1f433e2262b46fc062fe32f45f780140a540dd3
wang22 <pause22@163.com>
2018-09-08 14:13:11 +0800

    auth login page

--- src/layout/auth.vue
+++ src/layout/auth.vue
@@ -1,9 +1,61 @@
 <template>
     <div class="curtain">
-        <div class="main"><router-view/></div>
+        <div class="main">
+            <div class="left">
+                <div class="text">
+                    <p>Amazing framework with tons of options! It helped us build our project! Jack Estrada, Web Developer</p>
+                </div>
+            </div>
+            <div class="right"><router-view/></div>
+        </div>
     </div>
 </template>
 
 <style lang="less" scoped>
+@base-width: 750px;
+@border-radius: 5px;
 
+.curtain {
+    background: #e6ebf4;
+    height: 100vh;
+    widows: 100%;
+    .main {
+        position:absolute;
+        top: 150px;
+        left: 50%;
+        margin-left: -@base-width/2;
+        width: @base-width;
+        background: #fff;
+        box-shadow: 0 0.5rem 2rem #d8dfed;
+        border-radius: @border-radius;
+        background-image: url('../assets/login_bg.jpg');
+        background-position: 0 50%;
+        background-size: cover;
+        .left {
+            position: absolute;
+            bottom: 0;
+            top: 0;
+            width: @base-width/2;
+            background-color: rgba(5,77,158,.8)!important;
+            border-top-left-radius: @border-radius;
+            border-bottom-left-radius: @border-radius;
+            .text {
+                position: absolute;
+                top: 20%;
+                padding: 30px;
+                font-size: 18px;
+                color: #fff;
+            }
+        }
+        .right {
+            float: right;
+            width: @base-width/2;
+            background: #fff;
+            padding: 20px;
+            padding-bottom: 0;
+            border-top-right-radius: @border-radius;
+            border-bottom-right-radius: @border-radius;
+        }
+    }
+}
 </style>
--- src/pages/auth/login.vue
+++ src/pages/auth/login.vue
@@ -1,13 +1,29 @@
 <template>
-    <Form ref="formCustom">
-        <FormItem>
-            <Input type="text"></Input>
-        </FormItem>
-        <FormItem>
-            <Input type="password"></Input>
-        </FormItem>
-        <FormItem>
-            <Button type="primary">Submit</Button>
-        </FormItem>
-    </Form>
+    <div>
+        <div class="logo"><span>Zing</span>CMS</div>
+        <Form ref="formCustom">
+            <FormItem>
+                <Input type="text" prefix="ios-contact-outline"></Input>
+            </FormItem>
+            <FormItem>
+                <Input type="password" prefix="ios-key-outline"></Input>
+            </FormItem>
+            <FormItem>
+                <Button type="primary" long>Submit</Button>
+            </FormItem>
+        </Form>
+    </div>
 </template>
+<style lang="less" scoped>
+.logo {
+    margin-top: 30px;
+    margin-bottom: 30px;
+    font-size: 30px;
+    font-weight: 600;
+    color: #0665d0!important;
+    text-align: center;
+    span {
+        color: #343a40!important;
+    }
+}
+</style>
